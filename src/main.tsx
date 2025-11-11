e9d168f8a17a1ed3835c9c7c61777a23f658fc19
valquirino <erca@cesar.school>
2025-11-11 17:09:05 -0300

    feat: add new routes for Estoques, Produtos, Fornecedores, Cotacoes, Pedidos, PontoRessuprimento, Alertas, Movimentacoes, Transferencias, and Reservas

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './views/Login/Login';
 import Cadastro from './views/Cadastro/Cadastro';
-import Home from './views/Home';
+import Estoques from './views/Estoques/Estoques';
+import Produtos from './views/Produtos/Produtos';
+import Fornecedores from './views/Fornecedores/Fornecedores';
+import Cotacoes from './views/Cotacoes/Cotacoes';
+import Pedidos from './views/Pedidos/Pedidos';
+import PontoRessuprimento from './views/PontoRessuprimento/PontoRessuprimento';
+import Alertas from './views/Alertas/Alertas';
+import Movimentacoes from './views/Movimentacoes/Movimentacoes';
+import Transferencias from './views/Transferencias/Transferencias';
+import Reservas from './views/Reservas/Reservas';
 
 function App(): React.ReactElement {
     return (
@@ -11,7 +20,16 @@ function App(): React.ReactElement {
                 <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/cadastro" element={<Cadastro />} />
-                <Route path="/home" element={<Home />} />
+                <Route path="/estoques" element={<Estoques />} />
+                <Route path="/produtos" element={<Produtos />} />
+                <Route path="/fornecedores" element={<Fornecedores />} />
+                <Route path="/cotacoes" element={<Cotacoes />} />
+                <Route path="/pedidos" element={<Pedidos />} />
+                <Route path="/ponto-ressuprimento" element={<PontoRessuprimento />} />
+                <Route path="/alertas" element={<Alertas />} />
+                <Route path="/movimentacoes" element={<Movimentacoes />} />
+                <Route path="/transferencias" element={<Transferencias />} />
+                <Route path="/reservas" element={<Reservas />} />
             </Routes>
         </BrowserRouter>
     );
